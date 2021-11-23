1b89bcd1998ae3e04f9d4db7ed5a98cfd3e1761c
Maewinn <maud.glacee@gmail.com>
2021-11-23 18:33:14 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+var http = require('http');
+var mysql = require('mysql');
+const util = require('util');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "user",
+    password: "password",
+    database: "db"
+  });
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log('successfully connected to mysql');
+  });
+
+const query = util.promisify(con.query).bind(con);
+
+http.createServer(async function (request, response) {
+
+    await query("UPDATE counterTable SET counter_count = counter_count+1 WHERE counter_id = 1");
+    const userCount = query('SELECT counter_count FROM counterTable WHERE counter_id = 1');
+
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+
+    response.write('Hello!\n');
+
+    response.write(' I have been seen '+userCount+' times!\n');
+
+    response.end();
+
+}).listen(3000);
+
+
+console.log('Server started run at the http://localhost:8000');
